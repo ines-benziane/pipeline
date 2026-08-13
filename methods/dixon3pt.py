7e31639891c7496645f95b85254a603f9b3a82c8
Ines Benziane <i.benziane@institut-myologie.org>
2026-08-13 16:23:41 +0200

    methods, dixon_3pt : using readers.py from mutools in dixon3pt, not our own implementation

--- methods/dixon3pt.py
+++ methods/dixon3pt.py
@@ -1,9 +1,10 @@
 """Dixon 3pt method"""
 
 from runner.method import Method
-from dicomstack import DicomStack, DICOM
+from dicomstack import DicomStack
 from mutools.fatwater.utils import make_mask, make_ffmap
 from mutools.fatwater.dixon import dixon_3pt
+from mutools.fatwater.readers import parse_dicom_dixon_default
 from musegai.io import Image
 import numpy as np
 from musegai.api import run_model
@@ -19,42 +20,8 @@ class Dixon3ptMethod(Method) :
         stack = DicomStack(exam_dir)
         if not stack :
             raise ValueError(f"No dicom data found in {exam_dir}")
-        EchoTime = stack.EchoTime
-        echo_times = sorted(stack.unique(EchoTime))
-        if not echo_times:
-            EchoTime = stack.EffectiveEchoTime
-            echo_times = sorted(stack.unique(EchoTime))
-        if not echo_times:
-            raise ValueError("Could not find echo-time values")
-        echo_times = [echo for echo in echo_times if not np.isclose(echo, 0)]
-        manufacturer = stack.single("Manufacturer")
-
-        if manufacturer.lower().startswith('siemens'):
-            image_type = {"selector": DICOM.ImageType[2], "magnitude": "M", "phase": "P"}
-            phase_scale = np.pi / 4096.0
-
-        elif manufacturer.lower().startswith('philips'):
-            image_type = {"selector": DICOM.ImageType[3], "magnitude": "M", "phase": "P"}
-            phase_scale =  1e-3
-        else:
-            raise ValueError(f"Manufacturer {manufacturer} not handled")
-
-        ImType = image_type["selector"]
-        volumes = []
-        for echo in echo_times:
-            mag = stack((EchoTime == echo) & (ImType == image_type["magnitude"])).as_volume(rescale=True)
-            pha = stack((EchoTime == echo) & (ImType == image_type["phase"])).as_volume(rescale=True)
-            volume = mag * np.exp(1j * pha * phase_scale)
-            volumes.append(volume)
-        sequence = stack.single(DICOM.SequenceName, default=None)
-        field_strength = stack.single(DICOM.MagneticFieldStrength, default=None)
-
-        info = {
-            "manufacturer": manufacturer,
-            "sequence": sequence,
-            "field_strength": field_strength,
-            "echo_times": echo_times,
-        }
+        info, volumes = parse_dicom_dixon_default(stack, npoint=3)
+        echo_times = info["echo_times"]
         mask = make_mask(*volumes, axis=2, threshold=10)
         water_map, fat_map, delta_b0, r2_star = dixon_3pt(echo_times, *volumes, mask = mask, force_reconstruction=False, global_swap=False)
         ffmap = make_ffmap(water_map, fat_map, mask=mask)
