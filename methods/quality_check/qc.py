1c934c5d3731a7eb831325e381232c3cf671cb13
Ines Benziane <i.benziane@institut-myologie.org>
2026-09-17 10:25:10 +0200

    Fixed: qc overview display + GIR orientation

--- methods/quality_check/qc.py
+++ methods/quality_check/qc.py
@@ -7,7 +7,7 @@ from mutools.utils.interpolate import interpolate_roi
 
 def quality_check_volumes(volume):
     volume = set_orientation(volume, "RAI")
-    image = volume_overview(volume)
+    image = volume_overview(volume, n=(2, 5))
     return image
 
 def quality_check_seg(volume, roi, labels, step=1):
@@ -19,7 +19,7 @@ def quality_check_seg(volume, roi, labels, step=1):
     roi = asvolume(roi.array, spacing=tuple(roi.spacing), origin=tuple(roi.origin),
                    transform=tuple(map(tuple, np.reshape(roi.transform, (3,3)).T)))
     roi = interpolate_roi(volume, roi)
-    images = [draw_image(volume[:, :, k], roi = np.asarray(roi[:, :, k]), labels=labels, roialpha=0.5, show_labels=False, vmin=vmin, vmax=vmax) for k in  range (0,volume.shape[2], step)]
+    images = [draw_image(volume[:, :, k], roi = np.asarray(roi[:, :, k]), labels=labels, roialpha=0.5, show_labels=False, vmin=vmin, vmax=vmax).rotate(90, expand=True) for k in  range (0,volume.shape[2], step)]
     return images
 
 def save_gif(frames, path, duration=100):
